85d43193de4c5863cc1dfcaed8b08ff5caf02397
RimshaZahidIqbal <zrimsha219@gmail.com>
2025-06-26 20:57:17 +0500

    show.js corrected

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("MongoDB connected");
-    } catch (err) {
-        console.error("Error connecting to MongoDB", err);
-        process.exit(1);
-    }
+    await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log("Database connected sucessfully");
+    }).catch(() => {
+        console.log("Connection Failed");
+    })
 };
 
 module.exports = connectDB;
